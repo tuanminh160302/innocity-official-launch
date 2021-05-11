450601288b9cee09320263ab15b9eca3331a2759
tuanminh160302 <canhtuan09@gmail.com>
2021-05-11 19:19:59 -0400

    route

--- js/general.js
+++ js/general.js
@@ -1,5 +1,5 @@
 document.querySelector('#logo').addEventListener('click', () => {
-    document.location.href = 'index.html'
+    document.location.href = 'index'
 })
 
 screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
