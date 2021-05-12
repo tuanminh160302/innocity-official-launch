5d0d09f7fa1cba2e1e54a3c7110856219b833e7d
tuanminh160302 <canhtuan09@gmail.com>
2021-05-11 20:06:22 -0400

    language

--- js/general.js
+++ js/general.js
@@ -1,10 +1,10 @@
-document.querySelector('#logo').addEventListener('click', () => {
-    console.log(language)
-    // if (language === 'vn') {
-    //     document.location.href = 'index'
-    // } else if (language === 'eng') {
-    //     document.location.href = 'eng-index'
-    // }
-})
+// document.querySelector('#logo').addEventListener('click', () => {
+//     console.log(language)
+//     // if (language === 'vn') {
+//     //     document.location.href = 'index'
+//     // } else if (language === 'eng') {
+//     //     document.location.href = 'eng-index'
+//     // }
+// })
 
-screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
+// screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
